85995fca90a12dbfe7b135da6786144cd990acc9
abdeljail <taki22abdo@gmail.com>
2022-09-26 18:56:57 +0100

    front end

---
+++ src/js/pages/admin/image.js
@@ -0,0 +1,8 @@
+import "../../../assets/css/image.css"
+import image from "../../../assets/images/upload-image.png"
+
+const upload_image = document.getElementById("image");
+
+console.log(image);
+
+upload_image.src = image;
